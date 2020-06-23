b2773c364926b3e06f05c5c1197b93a48b4bcff1
Katie Carroll <ktcarroll4@gmail.com>
2020-06-23 15:02:52 -0500

    added note main and note nav

---
+++ src/NoteListMain/NoteListMain.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Note from "../Note/Note";
+import CircleButton from "../CircleButton/CircleButton";
+import "./NoteListMain.css";
+
+export default function NoteListMain(props) {
+  return (
+    <section className="NoteListMain">
+      <ul>
+        {props.notes.map((note) => (
+          <li key={note.id}>
+            <Note id={note.name} name={note.name} modified={note.modified} />
+          </li>
+        ))}
+      </ul>
+      <div className="NoteListMain__button-container">
+        <CircleButtom
+          tag={Link}
+          to="/add-note"
+          type="button"
+          className="NoteListMain__add-note-button"
+        >
+          <FontAwesomeIcon icon="plus" />
+          <br />
+          Note
+        </CircleButtom>
+      </div>
+    </section>
+  );
+}
+
+NoteListMain.defaultProps = {
+  notes: [],
+};
