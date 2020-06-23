f3835e05895f1a2bc73b24e7052c93d514e3999d
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-23 14:13:53 -0700

    should work...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter } from 'react-router-dom'
+import 'typeface-roboto'
+import './index.css'
+import App from './App/App'
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
-);
-
+)
