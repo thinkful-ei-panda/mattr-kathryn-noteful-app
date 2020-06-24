944ab4187da7999ff0c9d9feb3f6557e654e29f7
Katie Carroll <ktcarroll4@gmail.com>
2020-06-24 09:30:27 -0500

    moved config and apicontext file

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
-import ApiContext from '../ApiContext'
-import config from '../config'
+import ApiContext from '../../ApiContext'
+import config from '../../config'
 import './AddFolder.css'
 
 export default class AddFolder extends Component {
--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
-import ApiContext from '../ApiContext'
-import config from '../config'
+import ApiContext from '../../ApiContext'
+import config from '../../config'
 import './AddNote.css'
 
 export default class AddNote extends Component {
